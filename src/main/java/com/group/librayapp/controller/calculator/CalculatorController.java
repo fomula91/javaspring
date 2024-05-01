e497998be801aaf690007cdb2d4e695e5d9509c7
KimHyeongJoong <fomula91@gmail.com>
2024-05-01 21:30:38 +0900

    feat: add calc api

--- src/main/java/com/group/librayapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/librayapp/controller/calculator/CalculatorController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.group.librayapp.dto.calculator.CalculatorAddRequest;
 import com.group.librayapp.dto.calculator.CalculatorMultiplyReqeust;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +23,16 @@ public class CalculatorController {
     @PostMapping("/multiply")
     public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyReqeust request) {
         return request.getNumber1() * request.getNumber2();
+    }
+
+    @GetMapping("/api/v1/calc")
+    public Map<String, Object> getCalc(CalculatorAddRequest request) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("add", request.getNumber1() + request.getNumber2());
+        result.put("minus", request.getNumber1() - request.getNumber2());
+        result.put("multiply", request.getNumber1() * request.getNumber2());
+
+        return result;
     }
     
 }
