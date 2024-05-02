7bcccb06f66c05092d2afb5d03f7558161f5d4d0
KimHyeongJoong <fomula91@gmail.com>
2024-05-03 02:11:18 +0900

    feat: Refactor UserController to use JdbcTemplate for database operations

--- src/main/java/com/group/librayapp/controller/user/UserController.java
+++ src/main/java/com/group/librayapp/controller/user/UserController.java
@@ -6,35 +6,48 @@ import com.group.librayapp.domain.user.User;
 import com.group.librayapp.dto.user.request.UserCreateRequest;
 import com.group.librayapp.dto.user.response.UserResponse;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @RestController
 public class UserController {
+    private final JdbcTemplate jdbcTemplate;
 
-    private final List<User> users = new ArrayList<>();
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
         
     }
     
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        List<UserResponse> responses = new ArrayList<>();
-        for (int i = 0; i < users.size(); i++) {
-            responses.add(new UserResponse(i+1, users.get(i)));
-        }
-        return responses;
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+            @Override
+            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                return new UserResponse(id, name, age);
+            }
+        });
+        
     }
     
 }
