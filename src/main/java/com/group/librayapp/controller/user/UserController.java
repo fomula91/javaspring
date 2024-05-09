349b45ee1fbea38ddd339b6035f37a92a2804ffc
KimHyeongJoong <fomula91@gmail.com>
2024-05-09 23:15:24 +0900

    feat: Add FruitService to UserController for fruit API integration

--- src/main/java/com/group/librayapp/controller/user/UserController.java
+++ src/main/java/com/group/librayapp/controller/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.group.librayapp.dto.user.request.UserCreateRequest;
 import com.group.librayapp.dto.user.request.UserUpdateRequest;
 import com.group.librayapp.dto.user.response.UserResponse;
+import com.group.librayapp.service.fruit.FruitService;
 import com.group.librayapp.service.user.UserService;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
--- src/main/java/com/group/librayapp/repository/user/UserRepository.java
+++ src/main/java/com/group/librayapp/repository/user/UserRepository.java
@@ -4,9 +4,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.group.librayapp.dto.user.response.UserResponse;
 
@@ -57,5 +60,5 @@ public class UserRepository {
             }
         });
     }
- 
+    
 }
